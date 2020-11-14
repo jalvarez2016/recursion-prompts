122c73b54361b332bde244e16aaa116dd62ba59c
jalvarez2016 <jasonalvarezcoraiza@gmail.com>
2020-11-13 19:20:56 -0500

    finished problem 1

--- src/recursion.js
+++ src/recursion.js
@@ -7,8 +7,9 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+    return (n<0) ? null : (n===0) ? 1 : (n === 1) ? n : factorial(n-1) * n;
 };
-
+console.log(factorial(5)); // 120
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
