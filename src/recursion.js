7ec8a001175a755643537b1147da6205f82704ec
jalvarez2016 <jasonalvarezcoraiza@gmail.com>
2020-11-16 19:37:52 -0500

    finished problem 9

--- src/recursion.js
+++ src/recursion.js
@@ -109,6 +109,10 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+    if(string.length === 1){
+        return string;
+    }
+    return reverse(string.slice(1)) + string[0];
 };
 
 // 10. Write a function that determines if a string is a palindrome.
